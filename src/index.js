750afc8fc92fb72b363456c4221d21fdad708d86
Edi980 <beqirkelmendi8@gmail.com>
2026-08-08 12:21:49 +0000

    Downgrade to Express 4 to fix wildcard bugs

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ async function refreshTools() {
   }
 }
 
-app.options('/*', (req, res) => { setCors(res); res.status(204).end(); });
+app.options('*', (req, res) => { setCors(res); res.status(204).end(); });
 
 app.get('/health', (req, res) => {
   res.json({
